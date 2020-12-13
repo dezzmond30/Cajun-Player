d3a29e7edacc7159f3f0c075099e2b7ce6319069
Shahdad <kingkajind@gmail.com>
2020-12-13 12:41:04 +0100

    Two LEDs can run simultaneously

--- solenoid_player/solenoid_player.ino
+++ solenoid_player/solenoid_player.ino
@@ -1,12 +1,12 @@
 const int invalidIndex = -1;
 
 // Channel 1 variables
-const int ch1_pin =  13; // channel 1 solenoid pin on arduino
+const int ch1_pin = 13; // channel 1 solenoid pin on arduino
 unsigned long ch1_lastStepMillis_ms = 0; // timestamp for last step on channel 1
 int ch1_currentIndex = invalidIndex; // current step being played. invalidIndex specifies that the sequence is not being played
 
 // Channel 2 variables
-const int ch2_pin =  14; // channel 2 solenoid pin on arduino
+const int ch2_pin =  12; // channel 2 solenoid pin on arduino
 unsigned long ch2_lastStepMillis_ms = 0; // entry  timestamp for last step on channel 2
 int ch2_currentIndex = invalidIndex; // current step being played. invalidIndex specifies that the sequence is not being played
 
@@ -19,21 +19,21 @@ struct Contact
 };
 
 // Define each contact type
-Contact shortTap = { HIGH, 100 };
-Contact longTap = { HIGH, 200 };
-Contact shortPause = { LOW, 100};
-Contact longPause = { LOW, 200};
+Contact shortTap = { HIGH, 50 };
+Contact longTap = { HIGH, 400 };
+Contact shortPause = { LOW, 50};
+Contact longPause = { LOW, 400};
   
 // Define sequence for channel 1
-Contact ch1_seq[] = { shortTap, shortPause, shortTap, longPause, longTap, shortPause, shortTap, shortPause, shortTap, shortPause };
+Contact ch1_seq[] = { shortTap, shortPause, shortTap, shortPause, longTap, longPause, longTap, longPause, shortTap, shortPause };
 const int ch1_seqLen = sizeof(ch1_seq) / sizeof(Contact);
 
 // Define sequence for channel 2
-Contact ch2_seq[] = { shortTap, shortPause, shortTap, longPause, longTap, shortPause, shortTap, shortPause, shortTap, shortPause };
+Contact ch2_seq[] = { longTap, longPause, longTap, longPause, shortTap, shortPause, shortTap, shortPause, longTap, longPause, longTap, longPause };
 const int ch2_seqLen = sizeof(ch2_seq) / sizeof(Contact);
 
 // Button variables
-const int btnPin = 15;
+const int btnPin = 2;
 
 void setup()
 {
@@ -82,24 +82,25 @@ void loop()
   if(ch1_currentIndex != invalidIndex)
   {
     // Channel 1 sequence is being played
-
+    
     // Set correct pin state without condition
     digitalWrite(ch1_pin, ch1_seq[ch1_currentIndex].solState);
 
+    // Tracks the timeout period
     unsigned long ch1_currentStateTimeout = ch1_lastStepMillis_ms + ch1_seq[ch1_currentIndex].duration_ms;
-    if(ch1_currentStateTimeout >= currentMillis)
+    if(currentMillis >= ch1_currentStateTimeout)
     {
       // Current step finished
       ch1_currentIndex++;
 
       if(ch1_currentIndex < ch1_seqLen)
       {
-        Serial.print("Sequence 1 going to step ")
+        Serial.print("Sequence 1 going to step ");
         Serial.println(ch1_currentIndex);
         ch1_lastStepMillis_ms = currentMillis;
       }
     }
-
+  }
  
   // Handle channel 2
   if(ch2_currentIndex >= ch2_seqLen)
@@ -118,14 +119,14 @@ void loop()
     digitalWrite(ch2_pin, ch2_seq[ch2_currentIndex].solState);
 
     unsigned long ch2_currentStateTimeout = ch2_lastStepMillis_ms + ch2_seq[ch2_currentIndex].duration_ms;
-    if(ch2_currentStateTimeout >= currentMillis)
+    if(currentMillis >= ch2_currentStateTimeout)
     {
       // Current step finished
       ch2_currentIndex++;
 
-      if(ch2_currentIndex < ch1_seqLen)
+      if(ch2_currentIndex < ch2_seqLen)
       {
-        Serial.print("Sequence 2 going to step ")
+        Serial.print("Sequence 2 going to step ");
         Serial.println(ch2_currentIndex);
         ch2_lastStepMillis_ms = currentMillis;
       }
